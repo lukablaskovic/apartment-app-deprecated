d8659199d190c987414dbf9fe94560eb96aa68fd
lukablaskovic <lblaskovi@unipu.hr>
2022-04-22 23:38:18 +0200

    working on toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,138 @@
 <template>
+  <v-card class="mx-auto overflow-hidden" height="400" width="344">
+    <v-system-bar color="deep-purple darken-3"></v-system-bar>
+
+    <v-app-bar color="deep-purple accent-4" dark prominent>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>My files</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-filter</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute bottom temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-title>Foo</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Bar</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Fizz</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Buzz</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-card-text>
+      The navigation drawer will appear from the bottom on smaller size screens.
+    </v-card-text>
+  </v-card>
+</template>
+<!--
+<template>
   <v-app>
-    <v-main>
-      <v-footer color="primary lighten-1" padless>
-        <v-row justify="left" no-gutters>
-          <strong class="lighten-2 py-4 text-center white--text ml-2"
-            >Apartman Blašković - {{ new Date().getFullYear() }}
-          </strong>
-        </v-row>
-        <v-row justify="center" no-gutters>
-          <v-btn
-            v-for="link in links"
-            :key="link"
-            color="white"
-            text
-            rounded
-            class="my-4"
-          >
-            {{ link }}
-          </v-btn>
-        </v-row>
-      </v-footer>
-      <router-view />
-    </v-main>
+    <nav>
+      <v-main>
+        <v-app-bar color="primary " padless>
+          <v-app-bar-nav-icon
+            @click.stop="drawer = !drawer"
+          ></v-app-bar-nav-icon>
+          <v-toolbar-side-icon></v-toolbar-side-icon>
+          <v-row justify="left" no-gutters>
+            <strong
+              class="lighten-2 py-4 text-center white--text ml-2 text-uppercase"
+              >Welcome family {{ currentFamily }}!
+            </strong>
+          </v-row>
+          <v-row justify="center" no-gutters>
+            <v-btn
+              v-for="link in links"
+              :key="link"
+              color="white"
+              text
+              rounded
+              class="my-4"
+            >
+              {{ link }}
+            </v-btn>
+          </v-row>
+        </v-app-bar>
+        <v-navigation-drawer v-model="drawer" absolute bottom temporary>
+          <v-list nav dense>
+            <v-list-item-group
+              v-model="group"
+              active-class="deep-purple--text text--accent-4"
+            >
+              <v-list-item>
+                <v-list-item-title>Foo</v-list-item-title>
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-title>Bar</v-list-item-title>
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-title>Fizz</v-list-item-title>
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-title>Buzz</v-list-item-title>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-navigation-drawer>
+
+        <router-view />
+      </v-main>
+    </nav>
   </v-app>
 </template>
+-->
 
 <script>
 export default {
   name: "App",
+  watch: {
+    group() {
+      this.drawer = false;
+    },
+  },
 
   data: () => ({
-    obitelj: "Peikert",
+    drawer: false,
+    group: null,
+    currentFamily: "Peikert",
     links: [
-      "Početna",
-      "Usluge",
-      "Info",
-      "Zbrinjavanje otpada",
-      "Prijava štete",
-      "Kontaktirajte nas",
+      "HOME",
+      "SERVICES",
+      "INFO",
+      "WASTE DISPOSAL",
+      "DAMAGE REPORT",
+      "CONTACT US",
     ],
   }),
 };
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,26 @@
 <template>
-  <v-carousel cycle>
-    <v-carousel-item
-      v-for="(item, i) in items"
-      :key="i"
-      :src="require('@/assets/' + item.src)"
-      reverse-transition="fade-transition"
-      transition="fade-transition"
-    ></v-carousel-item>
-  </v-carousel>
+  <div class="">
+    <v-carousel cycle>
+      <v-carousel-item
+        v-for="(item, i) in items"
+        :key="i"
+        :src="require('@/assets/' + item.src)"
+        reverse-transition="fade-transition"
+        transition="fade-transition"
+      >
+        <div
+          class="flex justify-center items-center text-center md:text-6xl text-4xl"
+        >
+          <div
+            class="mt-12 bg-gradient-to-r from-lime-500 text-zinc-50 font-bold rounded-lg shadow-lg p-4"
+          >
+            Welcome to apartment Blašković!
+          </div>
+        </div>
+      </v-carousel-item>
+    </v-carousel>
+  </div>
 </template>
-
 <script>
 export default {
   name: "Home-page",
